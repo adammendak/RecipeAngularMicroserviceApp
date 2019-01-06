f9b535e4166e076df3899466ea774d72d3b35ab0
adammendak <adam.mendak@gmail.com>
2019-01-06 15:37:26 +0100

    more

---
+++ front-end/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe("schabowy", "kotlet jak nic", "https://static.smaker.pl/photos/f/8/3/f83c3e62c2a4e8ac6c400a7561fcd259_110705_57b32d412bf06_wm.jpg"),
+    new Recipe("schabowy", "kotlet jak nic", "https://static.smaker.pl/photos/f/8/3/f83c3e62c2a4e8ac6c400a7561fcd259_110705_57b32d412bf06_wm.jpg")
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
